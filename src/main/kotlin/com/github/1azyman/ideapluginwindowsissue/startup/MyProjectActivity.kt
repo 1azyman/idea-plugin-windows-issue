4fc47bc2bfa53e156392aaa9fab92ce1d98fd684
GitHub Action <action@github.com>
2025-08-01 10:05:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/1azyman/ideapluginwindowsissue/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/1azyman/ideapluginwindowsissue/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.1azyman.ideapluginwindowsissue.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
