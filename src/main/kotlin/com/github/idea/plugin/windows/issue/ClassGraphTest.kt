8e2dc095fe1a84857922673743ffcb2d788b4aad
Viliam Repan <vilo.repan@evolveum.com>
2025-08-01 14:17:23 +0200

    notes

--- src/main/kotlin/com/github/idea/plugin/windows/issue/ClassGraphTest.kt
+++ src/main/kotlin/com/github/idea/plugin/windows/issue/ClassGraphTest.kt
@@ -4,7 +4,9 @@ import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import io.github.classgraph.ClassGraph
 
-
+/**
+ * ClassGraphTest doesn't fail with 2024.3.6, but it does with 2025.1.4.1 on Windows.
+ */
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class ClassGraphTest : BasePlatformTestCase() {
 
