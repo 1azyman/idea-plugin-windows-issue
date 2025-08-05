0d7c7b0b7d8a98e7364d9f3102075cfd3a204773
Viliam Repan <vilo.repan@evolveum.com>
2025-08-05 12:32:48 +0200

    simplified test

--- src/test/kotlin/com/github/idea/plugin/windows/issue/ClassGraphTest.kt
+++ src/test/kotlin/com/github/idea/plugin/windows/issue/ClassGraphTest.kt
@@ -1,8 +1,11 @@
 package com.github.idea.plugin.windows.issue
 
+import com.github.idea.plugin.windows.issue.services.MyProjectService
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.intellij.util.lang.PathClassLoader
 import io.github.classgraph.ClassGraph
+import java.nio.file.FileSystems
 
 /**
  * ClassGraphTest doesn't fail with 2024.3.6, but it does with 2025.1.4.1 on Windows.
@@ -14,14 +17,24 @@ class ClassGraphTest : BasePlatformTestCase() {
 
     fun testClassGraphScan() {
         try {
-            val classes = ClassGraph()
-                .acceptPackages("io.github.classgraph")
-                .scan().use { scan ->
-                    val foundClasses = scan.allClasses.loadClasses()
-                    foundClasses.toSet()
-                }
+            val classLoader = MyProjectService::class.java.classLoader as PathClassLoader
+            val path = classLoader.files.get(0)
 
-            assertNotEmpty(classes)
+            println("Trying to create File from Path: " + path.toString())
+            println("Path filesystem: " + path.fileSystem + ", global file system: " + FileSystems.getDefault())
+
+            // this fails on windows/mac os
+            val file = path.toFile()
+
+            // original failing code
+//            val classes = ClassGraph()
+//                .acceptPackages("io.github.classgraph")
+//                .scan().use { scan ->
+//                    val foundClasses = scan.allClasses.loadClasses()
+//                    foundClasses.toSet()
+//                }
+//
+//            assertNotEmpty(classes)
         } catch (e: Exception) {
             e.printStackTrace()
             fail("Failed to load classes from classpath: ${e.message}")
